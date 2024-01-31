b38c9d2f59abade6db11f424454d827ef66b9931
Josh Nussbaum <joshnuss@gmail.com>
2024-01-31 17:37:05 +0800

    Lint

--- src/lib/server/billing.js
+++ src/lib/server/billing.js
@@ -31,12 +31,11 @@ export async function createCheckout(user, plan) {
 }
 
 export async function createPortalSession(user) {
-	return stripe.billingPortal.sessions.create({
-		customer: user.customerId,
-		return_url: absoluteURL('/dashboard')
-	})
+  return stripe.billingPortal.sessions.create({
+    customer: user.customerId,
+    return_url: absoluteURL('/dashboard')
+  })
 }
-
 
 function absoluteURL(path) {
   return new URL(path, env.DOMAIN).toString()
