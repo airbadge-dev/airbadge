6067f613c1897626c8f4f620040c1d5a03248b44
Josh Nussbaum <joshnuss@gmail.com>
2024-01-31 18:58:40 +0800

    Adds logic to get default plan

--- src/lib/server/plans.js
+++ src/lib/server/plans.js
@@ -1,5 +1,9 @@
 export function createPlans(plans) {
   return {
+    getDefault() {
+      return plans.find((plan) => plan.default)
+    },
+
     getByPriceId(priceId) {
       return plans.find((plan) => plan.priceId == priceId)
     }
--- src/lib/server/plans.test.js
+++ src/lib/server/plans.test.js
@@ -1,8 +1,8 @@
 import { createPlans } from './plans'
 
-const plans = createPlans([{ priceId: 'price_1234', name: 'Pro' }])
-
 describe('getByPriceId', () => {
+  const plans = createPlans([{ priceId: 'price_1234', name: 'Pro' }])
+
   test('when found, returns plan', () => {
     const result = plans.getByPriceId('price_1234')
 
@@ -11,6 +11,22 @@ describe('getByPriceId', () => {
 
   test('when not found, returns null', () => {
     const result = plans.getByPriceId('price_unknown')
+
+    expect(result).toBeUndefined()
+  })
+})
+
+describe('getDefault', () => {
+  test('when found, returns plan', () => {
+    const plans = createPlans([{ priceId: 'price_1234', name: 'Pro', default: true }])
+    const result = plans.getDefault()
+
+    expect(result).toMatchObject({ name: 'Pro' })
+  })
+
+  test('when not found, returns null', () => {
+    const plans = createPlans([{ priceId: 'price_1234', name: 'Pro' }])
+    const result = plans.getDefault()
 
     expect(result).toBeUndefined()
   })
