e46d826e5aa1518949578947f6c3c3bf0bce5727
Josh Nussbaum <joshnuss@gmail.com>
2024-02-08 11:12:24 +0800

    Lint

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,10 +7,14 @@ import { routes } from '$lib/server/routes'
 export { subscriber, nonSubscriber } from './server/guards'
 
 export function StripeSvelteKitAuth(options = {}) {
-  if (!options.plans || options.plans.length == 0) throw new Error('Must have at least one plan')
+  if (!options.plans || options.plans.length == 0)
+    throw new Error('Must have at least one plan')
+
   if (!options.providers || options.providers.length == 0)
     throw new Error('Must have at least one provider')
-  if (!options.adapter) throw new Error('An adapter is reqiured')
+
+  if (!options.adapter)
+    throw new Error('An adapter is reqiured')
 
   options.pages ||= {}
   options.pages.newUser ||= '/?event=new-user'
