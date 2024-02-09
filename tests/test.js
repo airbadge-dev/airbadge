b6863dfa7eedb5d5e6c7a6e7d4ed231365714b0a
Josh Nussbaum <joshnuss@gmail.com>
2024-02-09 20:30:51 +0800

    Adds integration tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,61 @@
 import { expect, test } from '@playwright/test'
+import { db } from './db.js'
+import { reset, createUser } from './factories.js'
 
-test('index page has expected h1', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
+await reset()
+
+test('sign in without subscription', async ({ page }) => {
+  const user = await createUser()
+
+  const session = await signIn(page, user)
+
+  expect(session).toMatchObject({
+    user: {
+      email: user.email,
+      name: user.name
+    }
+  })
+
+  expect(session.subscription).toBeUndefined()
 })
+
+test('sign in with subscription', async ({ page }) => {
+  const user = await createUser({
+    subscriptionId: 'sub_1234',
+    subscriptionStatus: 'ACTIVE',
+    customerId: 'cus_1234',
+    plan: 'pro'
+  })
+
+  const session = await signIn(page, user)
+
+  expect(session).toMatchObject({
+    user: {
+      email: user.email,
+      name: user.name,
+    },
+    subscription: {
+      id: 'sub_1234',
+      customerId: 'cus_1234',
+      status: 'ACTIVE',
+      plan: {
+        id: 'pro',
+        name: 'Pro',
+        price: 10000
+      }
+    }
+  })
+})
+
+async function signIn(page, user) {
+  await page.goto('/auth/signin')
+  await page.getByLabel('Username').fill(user.email)
+  await page.getByLabel('Password').fill('123456')
+  await page.getByRole('button').click()
+
+  const pre = await page.locator('pre')
+  const json = await pre.innerHTML()
+  const { session } = JSON.parse(json)
+
+  return session
+}
