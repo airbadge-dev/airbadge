669c409f01fb035252c1bf2df8e4d2fb654117fb
Josh Nussbaum <joshnuss@gmail.com>
2024-02-10 19:51:06 +0800

    Lint

--- apps/docs/src/lib/highlighter.js
+++ apps/docs/src/lib/highlighter.js
@@ -1,18 +1,18 @@
-import { codeToHtml } from 'shiki';
+import { codeToHtml } from 'shiki'
 
 export async function highlighter(code, lang) {
-	const html = await codeToHtml(code, {
-		lang,
-		theme: {
-			dark: 'github-nord'
-		}
-	});
+  const html = await codeToHtml(code, {
+    lang,
+    theme: {
+      dark: 'github-nord'
+    }
+  })
 
-	console.log(html);
+  console.log(html)
 
-	return escapeHtml(html);
+  return escapeHtml(html)
 }
 
 function escapeHtml(html) {
-	return html.replace(/{/g, '&lbrace;').replace(/}/g, '&rbrace;').replace(/`/g, '&grave;');
+  return html.replace(/{/g, '&lbrace;').replace(/}/g, '&rbrace;').replace(/`/g, '&grave;')
 }
--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,23 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
-import { mdsvex } from 'mdsvex';
-import { highlighter } from './src/lib/highlighter.js';
+import adapter from '@sveltejs/adapter-auto'
+import { mdsvex } from 'mdsvex'
+import { highlighter } from './src/lib/highlighter.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+  extensions: ['.svelte', '.md'],
 
-	preprocess: [
-		mdsvex({
-			extensions: ['.md'],
-			highlight: {
-				highlighter
-			}
-		})
-	],
+  preprocess: [
+    mdsvex({
+      extensions: ['.md'],
+      highlight: {
+        highlighter
+      }
+    })
+  ],
 
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: adapter()
+  }
+}
 
-export default config;
+export default config
