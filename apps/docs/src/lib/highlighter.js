cf9a19fcd7c7af69f7a74551e6197d79c5b1fed2
Josh Nussbaum <joshnuss@gmail.com>
2024-02-10 19:46:07 +0800

    Adds mdsvex and shiki to docs

---
+++ apps/docs/src/lib/highlighter.js
@@ -0,0 +1,21 @@
+import { codeToHtml } from 'shiki'
+
+export async function highlighter(code, lang) {
+	const html = await codeToHtml(code, {
+		lang,
+		theme: {
+			dark: 'github-nord'
+		}
+	})
+
+	console.log(html)
+
+	return escapeHtml(html)
+}
+
+function escapeHtml(html) {
+	return html
+		.replace(/{/g, '&lbrace;')
+		.replace(/}/g, '&rbrace;')
+		.replace(/`/g, '&grave;')
+}
--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,13 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
+import { mdsvex } from 'mdsvex'
+import { highlighter } from './src/lib/highlighter.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [
+		mdsvex({
+			extensions: ['.md'],
+			highlight: {
+				highlighter
+			}
+		})
+	],
+
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
 
-export default config;
+export default config
