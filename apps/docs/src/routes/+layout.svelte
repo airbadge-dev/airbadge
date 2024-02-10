8777efe106205537606b5cd8e4e60cff993c7519
Josh Nussbaum <joshnuss@gmail.com>
2024-02-11 01:37:41 +0800

    Adds layout and base style

---
+++ apps/docs/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<header>
+  AirBadge
+</header>
+
+<main>
+  <slot/>
+</main>
+
+<style>
+  @import "./global.css";
+
+  header {
+    background: var(--violet-4);
+    color: var(--gray-2);
+    padding: var(--size-2);
+  }
+
+  main {
+    padding: var(--size-3);
+    margin: var(--size-4) 0;
+  }
+</style>
