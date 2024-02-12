8c502e5a846704d100d903bfe0d0a26b571c54be
Josh Nussbaum <joshnuss@gmail.com>
2024-02-12 08:12:48 +0800

    docs: Adds rehype plugins for autolinking

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto'
 import { mdsvex } from 'mdsvex'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import { highlighter } from './src/lib/highlighter.js'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +11,10 @@ const config = {
   preprocess: [
     mdsvex({
       extensions: ['.md'],
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeAutolinkHeadings
+      ],
       highlight: {
         highlighter
       }
