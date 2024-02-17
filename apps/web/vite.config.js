3ec7bbe9f689fc7861194a3950fe70eb8dee337c
Josh Nussbaum <joshnuss@gmail.com>
2024-02-17 22:34:10 +0800

    web: Adds json5 support

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import json5 from 'vite-plugin-json5'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [json5(), sveltekit()],
+  resolve: {
+    alias: {
+      $data: path.resolve('./src/data')
+    }
+  }
 })
