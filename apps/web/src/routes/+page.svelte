760d23a4911ccef9392b0b5605b6051ef1878de1
Josh Nussbaum <joshnuss@gmail.com>
2024-02-27 23:24:58 +0800

    Adds temporary homepage

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<main>
+  <img src="/images/logo-violet.svg" alt="AirBadge logo"/>
+  <a href="https://docs.airbadge.dev">Documentation</a>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+    gap: var(--size-5);
+    margin: var(--size-5);
+  }
+
+  img {
+    width: max(300px, 40vw);
+  }
+
+  a {
+    font-size: var(--font-size-4);
+    color: var(--violet-4);
+  }
+</style>
