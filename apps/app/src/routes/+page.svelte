364723c471799df2118e2b7850cba8c80b76c3d4
Josh Nussbaum <joshnuss@gmail.com>
2024-03-15 16:48:01 +0800

    Adds link to billing portal

--- apps/app/src/routes/+page.svelte
+++ apps/app/src/routes/+page.svelte
@@ -1,3 +1,16 @@
+<script>
+  export let data
+
+  $: ({ session } = data)
+</script>
+
 <h1>Coming soon</h1>
 
 <p>This will contains docs & resources for paid plans.</p>
+
+{#if session?.subscription}
+  <nav>
+    <a href="/billing/portal">Billing Portal</a>
+    <a href="/auth/signout">Signout</a>
+  </nav>
+{/if}
