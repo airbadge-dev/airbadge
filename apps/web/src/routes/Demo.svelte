d6979a6df6a29702449347599afc3100407dcab1
Josh Nussbaum <joshnuss@gmail.com>
2024-04-29 19:32:20 -0400

    Adds code listing to demo section

--- apps/web/src/routes/Demo.svelte
+++ apps/web/src/routes/Demo.svelte
@@ -1,13 +1,35 @@
+<script>
+  const code = `import { SvelteKitAuth } from '@airbadge/sveltekit'
+
+export const handle = SvelteKitAuth({
+  adapter: PrismaAdapter(...),
+
+  providers: [
+    Google(...),
+    Apple(...),
+  ],
+
+  plans: [
+    { id: "basic", priceId: 'price_X213DS3'},
+    { id: "pro", priceId: 'price_4F32FA6'}
+  ]
+})
+`
+</script>
 <section id="#demo">
   <hgroup>
-  <span class="pill"><span>Demo</span></span>
-  <h2 class="text-gradient">Implement AirBadge in any application in just five minutes</h2>
-  <p>With a few lines of code you can have Auth0 integrated in any app written in any language, and any framework. We provide 30+ SDKs & Quickstarts to help you succeed on your implementation</p>
+    <span class="pill"><span>Demo</span></span>
 
-  <a class="btn" href="https://docs.airbadge.dev">
-    Go to documentation
-  </a>
+    <h2 class="text-gradient">Implement AirBadge in any application in just five minutes</h2>
+
+    <p>With a few lines of code you can have Auth0 integrated in any app written in any language, and any framework. We provide 30+ SDKs & Quickstarts to help you succeed on your implementation</p>
+
+    <a class="btn" href="https://docs.airbadge.dev">
+      Go to documentation
+    </a>
   </hgroup>
+
+  <pre>{code}</pre>
 </section>
 
 <style>
@@ -16,10 +38,10 @@
   section {
     --block-align-items: center;
     --block-align-text: center;
-    --block-padding: 0 0 10rem 0;
+    --block-padding: 0 0 4rem 0;
 
     @media (--sm-n-above) {
-      --block-padding: 0 0 8rem 0;
+      --block-padding: 0 0 6rem 0;
     }
 
     @media (--md-n-above) {
@@ -52,5 +74,14 @@
     & * {
       text-align: var(--block-align-text);
     }
+  }
+
+  pre {
+    width: calc(var(--screen-width) - 3rem);
+    padding: var(--size-3);
+    border: solid 1px var(--gray-7);
+    border-radius: var(--radius-2);
+    background: linear-gradient(180deg, #0A0A0D 5%, #15151F 50%);
+    overflow: scroll;
   }
 </style>
