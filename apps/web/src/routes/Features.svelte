df3fdb4376a9a47a441993e409558f42a055ba0b
Josh Nussbaum <joshnuss@gmail.com>
2024-05-02 23:57:19 -0400

    Adds images to feature section

--- apps/web/src/routes/Features.svelte
+++ apps/web/src/routes/Features.svelte
@@ -1,25 +1,33 @@
 <script>
+  import Fingerprint from './features/Fingerprint.svelte'
+  import Cube from './features/Cube.svelte'
+  import Team from './features/Team.svelte'
+
   const features = [
     {
       title: 'Payment integration',
-      description: 'Based on Stripe Checkout. AirBadge adds a playment step to the signup flow. It works for subscriptions and one-off purchases.'
+      description: 'Based on Stripe Checkout. AirBadge adds a playment step to the signup flow. It works for subscriptions and one-off purchases.',
+      component: Fingerprint
     },
     {
       title: 'Authentication',
       description: 'Built on top of Auth.js, AirBadge supports over 50+ authentication methods, including OAuth, SAML, passwords and passwordless logins.',
-      image: '/images/fingerprint.svg'
+      component: Fingerprint
     },
     {
       title: 'Minimal code',
-      description: 'It takes very little code to get started. That means less to learn and less to maintain.'
+      description: 'It takes very little code to get started. That means less to learn and less to maintain.',
+      component: Fingerprint
     },
     {
       title: 'Open Source & Self-hostable',
-      description: "Deploys to any hosting platform, including serverless or Docker-based platforms. All data lives in your own database. The code is fully open-source, so it's easy to modify or contribute"
+      description: "Deploys to any hosting platform, including serverless or Docker-based platforms. All data lives in your own database. The code is fully open-source, so it's easy to modify or contribute",
+      component: Cube
     },
     {
       title: 'Team support',
-      description: 'Coming soon. Share one subscription between multiple users. With the ability to increase the monthly charge based on number of seats used.'
+      description: 'Coming soon. Share one subscription between multiple users. With the ability to increase the monthly charge based on number of seats used.',
+      component: Team
     },
   ]
 </script>
@@ -39,9 +47,16 @@
 
     <div class="features">
       {#each features as feature}
-        <div class="feature">
+        <div class="feature"
+          on:mouseover={() => feature.active = true}
+          on:mouseout={() => feature.active = false}
+          on:focus={() => feature.active = true}
+          on:blur={() => feature.active = false}
+        >
           <h3 class="text-shadow">{feature.title}</h3>
           <p>{feature.description}</p>
+
+          <svelte:component this={feature.component} active={feature.active}/>
         </div>
       {/each}
     </div>
@@ -114,13 +129,22 @@
   }
 
   .feature {
+    position: relative;
     border: solid 1px var(--gray-9);
     border-radius: var(--radius-3);
     background: linear-gradient(180deg, #0A0A0E, #15151F);
     padding: var(--size-6);
+    padding-bottom: 10rem;
     display: flex;
     flex-direction: column;
     gap: var(--size-4);
+    overflow: hidden;
+    transition: all 0.2s var(--ease-2);
+
+    &:hover {
+      border-color: var(--gray-8);
+      background: linear-gradient(180deg, #0A0A0E 40%, #15151F);
+    }
   }
 
   .feature:nth-child(4) {
@@ -134,5 +158,13 @@
   .feature p {
     max-width: 40ch;
     font-size: 0.9rem;
+  }
+
+  .feature :global(svg) {
+    --transition: all 0.4s var(--ease-in-out-2);
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    transition: var(--transition);
   }
 </style>
---
+++ apps/web/src/routes/features/Cube.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let active
+</script>
+
+<svg class:active width="218" height="257" viewBox="0 0 218 257" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M250.793 73.1307L126.016 145.342L0.413574 73.1307L125.19 0.948975L250.793 73.1307Z" fill="#D9D0FF"/>
+  <path d="M250.379 216.818L125.603 289V145.422L250.792 73.1306L250.379 216.818Z" fill="#C5BFDF"/>
+  <path d="M126.016 145.342L125.603 289L0 216.818L0.413167 73.1306L126.016 145.342Z" fill="#9F96C4"/>
+  <path d="M190.04 108.371L125.869 145.357L61.2734 108.371L125.444 71.4001L190.04 108.371Z" fill="#A893FF"/>
+  <path d="M189.828 181.967L125.657 218.938V145.398L190.04 108.371L189.828 181.967Z" fill="#8468F6"/>
+  <path d="M125.87 145.357L125.657 218.938L61.0615 181.967L61.274 108.371L125.87 145.357Z" fill="#6741D9"/>
+</svg>
+
+<style>
+  path {
+    filter: grayscale(1) brightness(0.9);
+    transition: var(--transition);
+  }
+  .active path {
+    filter: brightness(1);
+  }
+</style>
