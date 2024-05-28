c6a9eab3da272ad9c240f301d1b4675af7a44eba
Josh Nussbaum <joshnuss@gmail.com>
2024-05-28 15:48:32 -0400

    Improves cli tests

--- packages/cli/src/bin.test.js
+++ packages/cli/src/bin.test.js
@@ -1,17 +1,25 @@
 import { spawnSync } from 'node:child_process'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 function exec(options) {
-  return spawnSync('node', ['./src/bin.js', ...options])
+  const binPath = path.join(__dirname, 'bin.js')
+
+  return spawnSync('node', [binPath, ...options])
 }
 
-test('has setup stripe command', async () => {
+test('has "setup stripe" command', async () => {
   const pid = exec(['setup', 'stripe', '--help'])
+  console.log(pid.stderr.toString())
   const stdout = pid.stdout.toString()
 
   expect(stdout).toMatch('Sets up Stripe environment')
 })
 
-test('has help command', async () => {
+test('has "--help" option', async () => {
   const pid = exec(['--help'])
   const stdout = pid.stdout.toString()
 
